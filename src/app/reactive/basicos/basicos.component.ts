9fd2f146473b91197503fb7fda381a6efb1edf0e
Daniel Ramirez <dani_0318@hotmail.com>
2022-12-28 23:59:59 -0500

    257 validando campos de formulario reactivo basico

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   'nombre': new FormControl('RTX 4080ti'),
+  //   'precio': new FormControl('1500'),
+  //   'existencia': new FormControl('5'),
+  // });
 
-  ngOnInit(): void {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ , [Validators.required, Validators.minLength(3)] ],
+    precio: [ , [Validators.required, Validators.min(0)] ],
+    existencia: [ , [Validators.required, Validators.min(0)] ]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  campoEsValido(campo:string){
+
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+
   }
+
 
 }
