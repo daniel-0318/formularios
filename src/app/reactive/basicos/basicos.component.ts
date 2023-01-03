77b5bd3ec2783d0387a7d3fe57296e5cc82ac925
Daniel Ramirez <dani_0318@hotmail.com>
2023-01-02 22:48:33 -0500

    261 uso de formArray para crear un arreglo de formularios validados dinamicamente

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit{
 
   // miFormulario: FormGroup = new FormGroup({
   //   'nombre': new FormControl('RTX 4080ti'),
@@ -18,15 +18,34 @@ export class BasicosComponent {
   miFormulario: FormGroup = this.fb.group({
     nombre: [ , [Validators.required, Validators.minLength(3)] ],
     precio: [ , [Validators.required, Validators.min(0)] ],
-    existencia: [ , [Validators.required, Validators.min(0)] ]
+    existencias: [ , [Validators.required, Validators.min(0)] ]
   });
 
   constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080ti',
+      precio: 1600
+    });
+  }
 
   campoEsValido(campo:string){
 
     return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
 
+  }
+
+  guardar(){
+
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario);
+    this.miFormulario.reset();
+  
   }
 
 
