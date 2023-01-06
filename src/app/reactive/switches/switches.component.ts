c3ff98641a29b53a0bddedde4cfdf6a41c966e61
Daniel Ramirez <dani_0318@hotmail.com>
2023-01-05 22:47:06 -0500

    terminado de validaciones

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -27,6 +27,34 @@ export class SwitchesComponent implements OnInit{
       ...this.persona,
       condiciones: false
     })
+
+    // this.miFormulario.get('condiciones')?.valueChanges.subscribe(newValue=>{
+    //   console.log(newValue);
+    // });
+
+    this.miFormulario.valueChanges.subscribe(form =>{
+      console.log(form);
+      delete form.condiciones;
+      this.persona = form;
+      
+    });
+
+    // lo mismo de arriba solo que extraigo "condiciones" como variable aparte y lo demas en otra
+    // this.miFormulario.valueChanges.subscribe(({condiciones, ...rest}) =>{
+    //   console.log(rest);
+    //   this.persona = rest;
+      
+    // });
+  }
+
+  guardar(){
+
+    const formValue = {... this.miFormulario.value};
+
+    delete formValue.condiciones; //Eliminar campo, propio de javascript
+
+    this.persona = formValue
+
   }
 
   
